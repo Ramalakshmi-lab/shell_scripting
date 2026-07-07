a318193ef7779cf120680ade52c89d13ec7719f9
Githubrambo@123 <20a91a0579@aec.edu.in>
2026-07-07 16:49:55 +0530

    color

--- color.sh
+++ color.sh
@@ -3,15 +3,18 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 echo "$LOGFILE"
+R=\e[31m
+G=\e[32m
+N=\e[0m
 
 validate()
 {
     if [ $1 -ne 0 ]
     then 
-        echo -e "\e[31m $2 ..failed\e[0m"
+        echo -e " $R $2 ..failed $N"
         exit 1
     else
-        echo -e "\e[32m $2..success\e[0m"
+        echo -e " $G $2..success $N"
     fi
 
 }
