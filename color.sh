ebe36c151fad4b61982678227fa53732b2c70690
Githubrambo@123 <20a91a0579@aec.edu.in>
2026-07-07 16:34:59 +0530

    color

---
+++ color.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+echo "$LOGFILE"
+
+validate()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "\e[31m$2 ..failed"
+        exit 1
+    else
+        echo "\e[32m$2..success"
+    fi
+
+}
+
+user=$(id -u)
+if [ $user -ne 0 ]
+then 
+    echo "user sudo su"
+else
+    dnf install mariadb105-serve -y &>>$LOGFILE
+    validate $? "mysql installation"
+fi
+    
