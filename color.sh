96aabd98c47a79c6c87672eaaff57e1d67f57c38
Githubrambo@123 <20a91a0579@aec.edu.in>
2026-07-07 16:50:57 +0530

    color

--- color.sh
+++ color.sh
@@ -3,9 +3,9 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 echo "$LOGFILE"
-R=\e[31m
-G=\e[32m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 validate()
 {
