c56291c99ffccae5fb4682ae431a70d90095e053
Githubrambo@123 <20a91a0579@aec.edu.in>
2026-07-07 15:50:21 +0530

    fun2.sh

---
+++ fun2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+validate()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 ..failed"
+    else
+        echo "$2..success"
+    fi
+
+}
+
+user=$(id -u)
+if [ $user -ne 0]
+then 
+    echo "user sudo su"
+else
+    dnf install mariadb105-server -y
+    validate $? "mysql instalation"
+fi
+    
