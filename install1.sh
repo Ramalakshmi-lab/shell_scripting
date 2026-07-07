4529d9cf88de0a37b3ec9805f927adaccb109524
Githubrambo@123 <20a91a0579@aec.edu.in>
2026-07-07 15:41:30 +0530

    install1

---
+++ install1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+validate()
+{
+    echo "$!"
+    echo "$2"
+}
+user=$(id -u)
+if [ $user -ne 0 ]
+then
+    echo "use sudo su"
+else
+    echo "installation started..."
+    dnf install mariadb105-server -y
+    validate $? "installation done"
+fi
+
+    
