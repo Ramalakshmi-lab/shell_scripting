e96cdbe12eb41081545bcd42481b5a243a4252ae
Githubrambo@123 <20a91a0579@aec.edu.in>
2026-07-07 22:20:58 +0530

    loop1

--- loop1.sh
+++ loop1.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+validate()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 ..failed"
+        exit 1
+    else
+        echo "$2..success"
+    fi
+
+}
 user=$(id -u)
 if [ $user -ne 0 ]
 then 
@@ -11,4 +25,11 @@ fi
 for i in $@
 do 
     echo "install: $i"
+    dnf list install $i &>>LOGFILE 
+    if [ $i -eq 0 ]
+    then 
+        echo "$i package already installed"
+    else
+        echo "$i package need to be installed"
+    fi
 done
