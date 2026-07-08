c6ca453f2c1abf532dbd2a55d0ffe9d70a5a89c3
Githubrambo@123 <20a91a0579@aec.edu.in>
2026-07-08 11:13:29 +0530

    loop1

--- loop1.sh
+++ loop1.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-validate()
-{
-    if [ $1 -ne 0 ]
-    then 
+validate() {
+    if [ $1 -ne 0 ]; then 
         echo "$2 ..failed"
         exit 1
     else
-        echo "$2..success"
+        echo "$2 ..success"
     fi
-
 }
+
 user=$(id -u)
-if [ $user -ne 0 ]
-then 
-    echo "user sudo su"
+if [ $user -ne 0 ]; then 
+    echo "Run as super user (sudo su)"
     exit 1
 else 
-    echo "installed using suoer user"
+    echo "Running with super user access"
 fi
-for i in $@
-do 
-    echo "install: $i"
-    dnf list installed -y $i &>>$LOGFILE 
-    if [ $i -eq 0 ]
-    then 
+
+for i in "$@"; do 
+    echo "Checking: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]; then 
         echo "$i package already installed"
     else
-        echo "$i package need to be installed"
+        echo "$i package needs to be installed"
+        dnf install -y $i &>>$LOGFILE
+        validate $? "$i installation"
     fi
 done
